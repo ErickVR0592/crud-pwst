6468a46f701dd4704213e3db69eeaafdf3a29b7e
Erick Vazquez <erick.vr@live.com.mx>
2022-01-22 16:16:24 -0600

    [add] devDependencie fontawesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import "bulma/bulma.sass";
+import '@fortawesome/fontawesome-free/js/all'
 
 Vue.config.productionTip = false
 
