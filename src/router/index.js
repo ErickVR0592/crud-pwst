93438d36a4f60d9426f677bbb1a2c20a406f8cc6
Erick Vazquez <erick.vr@live.com.mx>
2022-01-22 16:30:18 -0600

    [chg] path and name routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/HomeLogin.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'HomeLogin',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/dashboard',
+    name: 'dashboard',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
