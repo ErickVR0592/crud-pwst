3399b7ff2bef564c0548b0f1e091b4688143768e
Erick Vazquez <erick.vr@live.com.mx>
2022-01-22 16:25:38 -0600

    [update] Home view to HomeLogin view

---
+++ src/views/HomeLogin.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login">
+    <!-- <img alt="Vue logo" src="../assets/logo-powerstreet.png"> -->
+    <section class="hero is-fullheight">
+      <div class="hero-body is-flex is-justify-content-center">
+        <Login msg="Bienvenido"/> 
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '@/components/Login.vue'
+
+export default {
+  name: 'HomeLogin',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style scoped>
+.hero{
+  background-image: url('../assets/background.jpg');
+  background-position-x: 75%;
+  background-position-y: 15%;
+}
+</style>
