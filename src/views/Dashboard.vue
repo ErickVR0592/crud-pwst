b0c8927668a731812401b4410d2daeff2a9a4773
Erick Vazquez <erick.vr@live.com.mx>
2022-01-24 15:45:22 -0600

    [add] Dashboard view

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Header/>
+
+    <div class="container mt-6">
+      <div class="buttons are-large is-flex is-justify-content-center">
+        <a class="button is-info is-fullwidth is-rounded" href="/products">Productos</a>
+        <a class="button is-success is-fullwidth is-rounded" href="/shopping">Compras</a>
+        <a class="button is-warning is-fullwidth is-rounded" href="/users">Usuarios</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Header from '../components/Header.vue'
+  export default {
+  name: 'Dashboard',
+  components: { Header },
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
