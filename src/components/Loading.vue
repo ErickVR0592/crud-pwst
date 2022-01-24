c42190f793696573bb1fcb4193717b5df6c9565f
Erick Vazquez <erick.vr@live.com.mx>
2022-01-24 15:47:48 -0600

    [add] Loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="columns is-multiline loading-component">
+    <div class="column is-12 has-text-centered is-flex is-flex-direction-column is-align-items-center">
+      <img src="../assets/logo-powerstreet.png" alt="PSWT logo">
+      <i class="fas fa-spinner"></i>
+    </div>
+    <div class="column is-12">
+      <div class="loading">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.loading-component {
+  margin-top: 150px;
+}
+img {
+  width: 250px;
+}
+.fa-spinner{
+  animation-name: rotate-loading;
+  animation-duration: 2s; 
+  animation-iteration-count: infinite; 
+  animation-timing-function: linear; 
+}
+
+@keyframes rotate-loading{
+  from{
+    transform: rotateZ(0deg);
+  }
+  to{
+    transform: rotateZ(360deg);
+  }
+}
+</style>
