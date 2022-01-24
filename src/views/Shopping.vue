4979b598d3f531a6a8b20aec5ed5f9b3e95c52d0
Erick Vazquez <erick.vr@live.com.mx>
2022-01-24 15:50:51 -0600

    [add] view component

---
+++ src/views/Shopping.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header/>
+    <a class="button is-info mb-6" onclick="window.history.back()">Go back</a>
+  </div>
+</template>
+
+<script>
+
+import Header from '../components/Header.vue'
+  export default {
+  name: 'Shopping',
+  components: { Header },
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
